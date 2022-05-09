1ca1bc2a3095933a261f2a0d82ef4c9bdeda5111
Bryce Dorn <bryce.dorn@uber.com>
2022-05-09 19:53:30 +0200

    use define instead of plugin

---
+++ build.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { build } = require('esbuild')
+
+const APP_BASE = 'src'
+const ENTRY_FILE = 'index.tsx'
+const OUTPUT_DIR = 'dist'
+const OUTPUT_FILE = 'index.js'
+
+build({
+  entryPoints: [`${APP_BASE}/${ENTRY_FILE}`],
+  define: {
+    'process.env.COOKIE': JSON.stringify(process.env.COOKIE),
+  },
+  bundle: true,
+  sourcemap: true,
+  minify: true,
+  outfile: `${OUTPUT_DIR}/${OUTPUT_FILE}`,
+})
+  .then(() => { 
+    console.log("Build succeeded.") })
+  .catch((e) => {
+    console.error("Error building:", e.message)
+    process.exit(1)
+  })
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-// @ts-ignore
-import { COOKIE } from 'env'
 import type { PostType, PostDetailType } from './types'
 
 const API_URL = 'https://dev.to/api'
@@ -7,7 +5,7 @@ const API_URL = 'https://dev.to/api'
 const headers = {
   'content-type': 'application/json',
   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
-  cookie: COOKIE,
+  cookie: process.env.COOKIE
 }
 
 const opts = {
