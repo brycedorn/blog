fd92139efc9551b5419c986ddca7a291baf0d191
Bryce Dorn <bryce.dorn@uber.com>
2022-06-04 22:47:03 +0200

    batched caching

--- src/utils.ts
+++ src/utils.ts
@@ -35,6 +35,12 @@ export async function getCachedPost(slug: string): Promise<PostDetailType> {
   return JSON.parse(response)
 }
 
+export async function getCachedSlugById(id: number) {
+  const post = await getPost(id)
+  const slug = cleanSlug(post.slug)
+  return slug
+}
+
 export function cleanSlug(slug: string): string {
   const indexOfLastDash = slug.split('').reverse().indexOf('-') + 1
   return slug.substr(0, slug.length - indexOfLastDash)
@@ -43,30 +49,21 @@ export function cleanSlug(slug: string): string {
 export async function updateEdgeCache(password: string, username: string) {
   if (password === process.env.PASSWORD) {
     const posts = await getPosts(username)
-    const cachedPosts = await getCachedPosts()
-    const isCached = postId => cachedPosts?.find(cachedPost => (
-      cachedPost.id === postId && cachedPost.cachedSlug
-    ))
-    const newCachedPosts = posts.map(post => ({
-      ...post, cachedSlug: isCached(post.id) ? cleanSlug(post.slug) : null
+    const newPosts = posts.map(post => ({
+      ...post, cachedSlug: cleanSlug(post.slug)
     }))
-    const numCached = newCachedPosts.filter(({ cachedSlug }) => cachedSlug).length
-    await POSTS.put('INDEX', JSON.stringify(newCachedPosts))
-    return new Response(`Indexed ${posts.length} posts, ${numCached} were already cached.`)
+    await POSTS.put('INDEX', JSON.stringify(newPosts))
+    posts.forEach((post, i) => {
+      setTimeout(async () => {
+        const postDetail = await getPost(post.id)
+        const slug = cleanSlug(post.slug)
+        await POSTS.put(slug, JSON.stringify({ ...postDetail, slug }))
+      }, i * 1000)
+    })
+    return new Response(`Indexed ${posts.length} posts.`)
   } else {
     return new Response('Wrong password.')
   }
-}
-
-export async function updateEdgeCacheForPost(id: number) {
-  const post = await getPost(id)
-  const slug = cleanSlug(post.slug)
-  await POSTS.put(`${slug}`, JSON.stringify({ ...post, slug }))
-  const posts = await getCachedPosts()
-  const cachedPostIndex = posts.map(p => p.id).indexOf(id)
-  posts[cachedPostIndex].cachedSlug = slug
-  await POSTS.put('INDEX', JSON.stringify(posts))
-  return slug
 }
 
 export function withMinifiedStyles(css) {
